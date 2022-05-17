34eba6b92401670d19ce606e6770c1687ef951b9
Your Name <john.newby@tzconnect.com>
2022-05-17 13:47:26 +0200

    Initial commit

---
+++ cache-blocks.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+BLOCKS_DIR=/home/newby/chains/main/blocks
+NODE=http://localhost:10734
+TOP=$1
+TMPFILE=block-tmp-$$.json
+
+set -e
+
+cd $BLOCKS_DIR
+
+if [ -z $TOP ]; then
+    HEAD=`curl --silent $NODE/chains/main/blocks/head | jq .header.level`
+    echo $HEAD
+    TOP=$(($HEAD - 3))
+fi
+echo $TOP
+
+while true; do
+    CURRENT=$TOP
+    TOP=$((TOP-1))
+    if [ -f $TOP.json ]; then
+	echo "$TOP was done"
+       continue
+    fi;
+    echo "Caching level $CURRENT"
+    wget -q $NODE/chains/main/blocks/$CURRENT -O $TMPFILE
+    mv $TMPFILE $CURRENT.json
+    if [ $TOP == 0 ]; then
+	break
+    fi
+done
+       
+    
